0715d1bd29cfe7f57c20c2573d089c193dcb3d03
Adam Strickland <adam.strickland@gmail.com>
2024-05-01 15:54:45 -0400

    include kickstart plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,12 +2,26 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+local plugins = {
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+}
 
--- Iterate over all Lua files in the plugins directory and load them
 local plugins_dir = vim.fs.joinpath(vim.fn.stdpath 'config', 'lua', 'custom', 'plugins')
+local custom_modules = {}
 for file_name, type in vim.fs.dir(plugins_dir, { follow = true }) do
   if (type == 'file' or type == 'link') and file_name:match '%.lua$' and file_name ~= 'init.lua' then
-    local module = file_name:gsub('%.lua$', '')
-    require('custom.plugins.' .. module)
+    custom_modules[#custom_modules + 1] = file_name:gsub('%.lua$', '')
   end
 end
+
+table.sort(custom_modules)
+for _, module in ipairs(custom_modules) do
+  plugins[#plugins + 1] = require('custom.plugins.' .. module)
+end
+
+return plugins
