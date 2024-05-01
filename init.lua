9ebe1a4c18b9556ba96e905cd10abe33310cbf2e
Adam Strickland <adam.strickland@gmail.com>
2024-05-01 16:20:33 -0400

    consolidate custom config

--- init.lua
+++ init.lua
@@ -908,8 +908,7 @@ require('lazy').setup({
   },
 })
 
-require 'custom.keymaps'
-require 'custom.options'
+require 'custom'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/init.lua
@@ -0,0 +1,8 @@
+-- require 'custom.plugins'
+
+-- require('lazy').setup {
+--   { import = 'custom.plugins' },
+-- }
+
+require 'custom.keymaps'
+require 'custom.options'
